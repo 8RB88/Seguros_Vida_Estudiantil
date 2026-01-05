b331819190932ee0a497ef010ecc5c74d576e04a
8RB88 <bustamanteroberto49@gmail.com>
2026-01-05 16:42:39 -0500

    conexion para backend y funcionalidades

--- pya/src/app/app.routes.ts
+++ pya/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './pages/login/login';
 import { UserDashComponent } from './pages/user-dash/user-dash';
 import { InsurerDashComponent } from './pages/insurer-dash/insurer-dash';
 import { AdminDashComponent } from './pages/admin-dash/admin-dash';
+import { RegistroSiniestroComponent } from './pages/registro-siniestro/registro-siniestro';
+import { DocumentosComponent } from './pages/documentos/documentos';
 
 // AGREGA 'export' antes de 'const'
 export const routes: Routes = [
@@ -10,6 +12,8 @@ export const routes: Routes = [
   { path: 'user-dashboard', component: UserDashComponent },
   { path: 'insurer-dashboard', component: InsurerDashComponent },
   { path: 'admin-dashboard', component: AdminDashComponent },
+  { path: 'registro-siniestro', component: RegistroSiniestroComponent },
+  { path: 'documentos', component: DocumentosComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
